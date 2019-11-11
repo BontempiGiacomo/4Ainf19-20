988cebb5f5768736c6ea56fe6e8207255625d327
BontempiGiacomocn <57103594+BontempiGiacomocn@users.noreply.github.com>
2019-11-11 11:20:13 +0100

    Add files via upload

---
+++ Informatica/017Porto/src/it/itis/cuneo/NaveMerci.java
@@ -0,0 +1,31 @@
+package it.itis.cuneo;
+
+/**
+ * Created by inf.bontempig3107 on 07/11/2019.
+ */
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class NaveMerci extends Nave {
+
+    public NaveMerci() {
+        super();
+    }
+
+    public NaveMerci(int tipoNave, float lunghezza, float larghezza, Date dataArrivo, int numeroPosizioneAttracco,
+                     String compagnia, GregorianCalendar tempoDiPermanenza) {
+        super(tipoNave, lunghezza, larghezza, dataArrivo, numeroPosizioneAttracco, compagnia, tempoDiPermanenza);
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void creaNave (Nave elencoNavi[], int cntNave, InputOutputUtility t) {
+        elencoNavi[cntNave] = new NaveMerci();
+        Nave.caricaNave(elencoNavi, cntNave, t);
+    }
+
+    public String toString() {
+        String s = new String(super.toString());
+
+        return s;
+    }
+}
